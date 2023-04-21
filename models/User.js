851b43288133e5e4a93a8cae0bf31afdb9ee6545
JanRezzonico <jan.rezzonico@samtrevano.ch>
2023-04-21 20:33:13 +0200

    Minor changes

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const schema = new mongoose.Schema({
     username: {
         required: true,
         type: String,
-        unique: true
+        unique: true,
+        lowercase: true
     },
     password: {
         required: true,
